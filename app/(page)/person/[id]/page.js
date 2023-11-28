01468527a7760c9b7d85224d8581f559f3aaca52
senthildeveloper <senthildeveloper4@gmail.com>
2023-11-28 19:00:03 +0530

    567

--- app/(page)/person/[id]/page.js
+++ app/(page)/person/[id]/page.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 export async function generateMetadata({params}){
     const person = await fetchData(`person/${params.id}`,'')
-    const image = person.profile_path
+    const image = person.profile_path.slice(1)
     const url = `https://movieuniverse.vercel.app_next/image?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F${image}&w=256&q=75`
     const dept = person?.known_for_department == 'Acting' && person?.gender == 2 ? 'Actor': '' || person?.known_for_department == 'Acting' && person?.gender == 1 ? 'Actress': ''|| person?.known_for_department == 'Writing' ? 'Writer': '' || person?.known_for_department == 'Directing' ? 'Director': '' || person?.known_for_department == 'Editing' ? 'Editor': '' || person?.known_for_department == 'Production' ? 'Producer': '' || person?.known_for_department == 'Sound' ? 'Music Composer': '' || person?.known_for_department == 'Camera' ? 'Cinematographer': '' || person?.known_for_department == 'Crew' ? 'Stunts': '' || person?.known_for_department == 'Art' ? 'Art Department': '' || person?.known_for_department == 'Visual Effects' ? 'VFX Artist': '' || person?.known_for_department == 'Lighting' ? 'Lighting Artist': '' || person?.known_for_department == 'Costume & Make-Up' ? 'Costume Designer': '';
     return {
@@ -24,9 +24,11 @@ const page = async ({params}) => {
         <p>hello {person.name}</p>
         <p> {person?.gender==1 && person?.known_for_department == 'Acting'  ? 'Actress' : dept }</p>
         <div className='flex gap-2 '><span>Born</span> <Dayjs res={person}/> </div>
-        <div className='md:flex mx-2 gap-2'>
-          <div className=' w-[200px] h-[300px]  '> 
-              <Image src={person.profile_path ? `https://image.tmdb.org/t/p/original${person.profile_path}`: "/defaultImage.jfif"} width={200} height={300} priority style={{objectFit:'cover',width:'200px',height:'300px'}} alt={person.name} className='rounded-sm'/>
+        <div className='md:flex mx-2 gap-2 w-full'>
+          <div className=' w-[200px] h-[300px]'>
+            <div className='relative   w-[200px] h-[300px]  '> 
+                <Image src={person.profile_path ? `https://image.tmdb.org/t/p/original${person.profile_path}`: "/defaultImage.jfif"} width={200} height={300} priority style={{objectFit:'cover',width:'200px',height:'300px'}} alt={person.name} className='rounded-sm'/>
+            </div>
           </div>
           <div className='overflow-hidden  text-sm '>
               {person.biography}
