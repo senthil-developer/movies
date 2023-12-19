7e2ae3de761e39f1c43960b303f79e378a201f45
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-19 21:45:03 +0530

    done

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,10 +14,10 @@ const Navbar = ({url}) => {
     // mobile navbar
     <div className='w-full flex header sticky bottom-0 md:hidden py-1 px-7 text-[10px] text-gray-400 bg-gray-800  items-center justify-between overflow-hidden z-50'>
       <Link className={url === '/' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/'>{url === '/' ? <FaHome size={25} /> : <GoHome size={25}/>}Home</Link>
-      <Link className={url === 'movie' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/movie'>{url == '/movie' ? <BiSolidMoviePlay size={25}/> : <BiMoviePlay size={25}/>}Movies</Link>
-      <Link className={url === 'search' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/search'>{url === '/search' ?<RiSearchFill size={25} />: <BsSearch size={25} className='p-0.5'/>}Search</Link>
-      <Link className={url === 'series' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/series'>{url === '/series' ? <PiTelevisionSimpleFill size={25}/> :<PiTelevisionSimple size={25}/>}Series</Link>
-      <Link className={url === 'person' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/person'>{url === '/person' ? <IoPeopleSharp size={25}/> : <IoPeopleOutline size={25}/>}Peoples</Link>
+      <Link className={url === 'movie' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/movie'>{url === 'movie' ? <BiSolidMoviePlay size={25}/> : <BiMoviePlay size={25}/>}Movies</Link>
+      <Link className={url === 'search' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/search'>{url === 'search' ?<RiSearchFill size={25} />: <BsSearch size={25} className='p-0.5'/>}Search</Link>
+      <Link className={url === 'series' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/series'>{url === 'series' ? <PiTelevisionSimpleFill size={25}/> :<PiTelevisionSimple size={25}/>}Series</Link>
+      <Link className={url === 'person' ? 'text-white flex flex-col items-center' : 'flex flex-col items-center'  } href='/person'>{url === 'person' ? <IoPeopleSharp size={25}/> : <IoPeopleOutline size={25}/>}Peoples</Link>
     </div>
   )
 }
