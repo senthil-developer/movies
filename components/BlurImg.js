eb1af0001007682f2c0df81ed527f14ba4542e9f
senthil-developer <senthildeveloper4@example.com>
2024-03-18 19:57:01 +0530

    check

--- components/BlurImg.js
+++ components/BlurImg.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { getPlaiceholder } from "plaiceholder";
 
 export default async function BlurImg({ src, alt }) {
-  const baseUrl = "https://image.tmdb.org/t/p/original";
+  const baseUrl = "https://image.tmdb.org/t/p/w500";
   const srcImg = "https://image.tmdb.org/t/p/w92" + src;
   const buffer = await fetch(srcImg).then(async (res) => {
     return Buffer.from(await res.arrayBuffer());
--- components/Tab.js
+++ components/Tab.js
@@ -21,37 +21,7 @@ const Tab = async ({ path, movie }) => {
       </div>
       <div className="w-full scroll-x flex gap-3 h-auto">
         {results?.map((result, i) => (
-          <>
-            {i < 10 && <Test key={result.id} results={result} />}
-            {i > 10 && (
-              <div className="flex gap-3 ml-2 my-2 w-full h-full pb-5">
-                <Link
-                  href={{
-                    pathname: `/${
-                      result.media_type === "tv" ? "series" : result.media_type
-                    }/${result.id}`,
-                    query: {
-                      name: `${String(result?.name || result?.title)}`,
-                    },
-                  }}
-                >
-                  <div className="relative w-[120px] h-[200px] sm:w-[150px] sm:h-[230px] md:[w-180px]  md:h-[250px] z-10 rounded-lg">
-                    <Image
-                      src={
-                        "https://image.tmdb.org/t/p/original" +
-                        result.poster_path
-                      }
-                      fill
-                      alt={result.name || result.title}
-                    />
-                  </div>
-                  <div className="text-sm">
-                    <Title res={result} length={[12, 16, 16]} />
-                  </div>
-                </Link>
-              </div>
-            )}
-          </>
+          <Test key={result.id} results={result} />
         ))}
       </div>
     </>
