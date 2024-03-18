27f766c8f12ca3dc2b947d89a3da88706583dc47
senthil-developer <senthildeveloper4@example.com>
2024-03-18 18:46:43 +0530

    check

--- components/BlurImg.js
+++ components/BlurImg.js
@@ -5,14 +5,24 @@ import { getPlaiceholder } from "plaiceholder";
 
 export default async function BlurImg({ src, alt }) {
   const baseUrl = "https://image.tmdb.org/t/p/original";
+  const srcImg = "https://image.tmdb.org/t/p/w92" + src;
+  const buffer = await fetch(srcImg).then(async (res) => {
+    return Buffer.from(await res.arrayBuffer());
+  });
 
+  const { base64 } = await getPlaiceholder(buffer, {
+    size: 4,
+  });
   return (
     <>
       <Image
         src={baseUrl + src}
         fill
         alt={alt}
+        placeholder="blur"
+        blurDataURL={base64}
         sizes="(max-width: 768px) 100vw,"
+        priority
       />
     </>
   );
