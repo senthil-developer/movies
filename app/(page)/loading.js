5ad4569442985cbd7f05c70cad4ad5639dc30ca0
senthil <senthildeveloper4@gmail.com>
2024-03-18 20:21:11 +0530

    Update (page)

---
+++ app/(page)/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+        <p className='mr-2'>Loading...</p>
+        <div className='animate-spin rounded-full h-10 w-10 border-b-2 border-red-700'>
+        </div>
+    </div>
+  )
+}
+
+export default Loading
