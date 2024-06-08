be6841f28679f2563a9bf8cc64177d48d2431b8f
senthil <senthildeveloper4@gmail.com>
2024-06-08 21:21:42 +0530

    Update BlurImg.js

--- components/BlurImg.js
+++ components/BlurImg.js
@@ -6,7 +6,7 @@ import { getPlaiceholder } from "plaiceholder";
 export default async function BlurImg({ src, alt }) {
   const Src = src.length > 15;
   const srcImg = Src ? "https://image.tmdb.org/t/p/w500" + src : src;
-  const blurImg = Src ? "https://image.tmdb.org/t/p/w92" + src : https://movieuniverse-nextjs.netlify.app + src
+  const blurImg = Src ? "https://image.tmdb.org/t/p/w92" + src : "https://movieuniverse-nextjs.netlify.app" + src
   const buffer = await fetch(blurImg).then(async (res) => {
     return Buffer.from(await res.arrayBuffer());
   });
@@ -20,8 +20,8 @@ export default async function BlurImg({ src, alt }) {
         src={srcImg}
         fill
         alt={alt}
-        //placeholder="blur"
-        //blurDataURL={base64}
+        placeholder="blur"
+        blurDataURL={base64}
         sizes="(max-width: 768px) 100vw,"
         className="rounded-md object-cover"
       />
